7976854d5e45da6e2a84806ee36aec496ca64816
terepoto <675135466@qq.com>
2019-06-04 16:30:18 +0900

    型制約を追加

--- B006.php
+++ B006.php
@@ -1,37 +1,38 @@
 <?php
+class B006
+{
+    private int $o_y;
 
-class B006{
+    private int $speed;
 
-    protected $o_y;
+    private int $theta;
 
-    protected $speed;
+    private int $distanceToTarget;
 
-    protected $theta;
+    private int $targetHeight;
 
-    protected $distanceToTarget;
+    private int $targetDiameter;
 
-    protected $targetHeight;
-
-    protected $targetDiameter;
-
-    public function __construct()
+    public function __construct($info_arrow, $infotarget)
     {
-        $info    = str_replace(array("\r\n","\r","\n"), '', fgets(STDIN));
+        $info    = str_replace(array("\r\n","\r","\n"), '', $info_arrow);
         $arrInfo = explode(" ", $info);
 
         $this->o_y   = $arrInfo[0];
         $this->speed = $arrInfo[1];
         $this->theta = $arrInfo[2];
 
-        $info = str_replace(array("\r\n","\r","\n"), '', fgets(STDIN));
+        $info = str_replace(array("\r\n","\r","\n"), '', $infotarget);
         $arrInfo = explode(" ", $info);
         $this->distanceToTarget = $arrInfo[0]; //的までの距離x
         $this->targetHeight     = $arrInfo[1]; //高さy
         $this->targetDiameter   = $arrInfo[2]; //的の大きさ
     }
 
-    public function checkIfHit()
+    public function checkIfHit() : string
     {
+        $result = "";
+
         $y = $this->o_y + tan(M_PI / 180 * $this->theta) * $this->distanceToTarget - ( 9.8 * pow($this->distanceToTarget, 2))
             / ( 2 * pow($this->speed, 2) * pow(cos(M_PI / 180 * $this->theta), 2) );
         $y = round($y, 1);
@@ -39,12 +40,14 @@ class B006{
         $targetRadius = $this->targetDiameter / 2;
 
         if ($this->targetHeight - $targetRadius > $y || $this->targetHeight + $targetRadius < $y) {
-            echo "Miss";
+            $result .= "Miss";
         } else {
-            echo "Hit" . " " . abs($this->targetHeight - $y);
+            $result .= "Hit" . " " . abs($this->targetHeight - $y);
         }
+
+        return $result;
     }
 }
 
-$B006 = new B006;
-$B006->checkIfHit();
+$B006 = new B006(fgets(STDIN), fgets(STDIN));
+echo $B006->checkIfHit();
