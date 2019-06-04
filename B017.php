27f4fd425826ae0b0ac173f47f0877473f522bdc
terepoto <675135466@qq.com>
2019-06-04 18:24:41 +0900

    修正

--- B017.php
+++ B017.php
@@ -13,20 +13,13 @@ class StrongestMeans
     private function countCard() : void
     {
         foreach ($this->cards as $card) {
-            /*switch ($card) {
-                case "*":
-                    $this->countOfCard = $this->plusAllKindCardOne($this->cards, $this->countOfCard);
-                    break;
-                default:
-                    $this->countOfCard[$card] = empty($this->countOfCard[$card]) ? 1 : $this->countOfCard[$card] + 1;
-                    break;
-            }*/
 
             if ($card == "*") {
                 $this->countOfCard = $this->plusAllKindCardOne($this->cards, $this->countOfCard);
-            } else {
-                $this->countOfCard[$card] = empty($this->countOfCard[$card]) ? 1 : $this->countOfCard[$card] + 1;
+                continue;
             }
+            $this->countOfCard[$card] = empty($this->countOfCard[$card]) ? 1 : $this->countOfCard[$card] + 1;
+
         }
     }
 
