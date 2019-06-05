c7eb010fca3f8cf304693cebc20ae28d66bd2fce
terepoto <675135466@qq.com>
2019-06-05 18:22:26 +0900

    大幅修正

--- B026.php
+++ B026.php
@@ -1,105 +1,114 @@
 <?php
-class B026
+class VendingMachine
 {
-    private array $countOfCoin;
+    private array $coinInMachine;
 
-    private int $countOfPeople;
-
-    public function __construct($info_coin, $info_people)
+    public function __construct($coinInMachine)
     {
-        $info = str_replace(array("\r\n","\r","\n"), '', $info_coin);
-        $arrInfo = explode(" ", $info);
-
-        $this->countOfCoin[500] = $arrInfo[0];
-        $this->countOfCoin[100] = $arrInfo[1];
-        $this->countOfCoin[50] = $arrInfo[2];
-        $this->countOfCoin[10] = $arrInfo[3];
-
-        $this->countOfPeople = str_replace(array("\r\n","\r","\n"), '', $info_people);
+        $this->coinInMachine = $coinInMachine;
     }
 
-    public function sell($info_customer) : string
+    public function sellResult(array $customers) : void
     {
-        $message = "";
-        $arrCustomerInfo = $this->getCustomerInfo($info_customer);
+        foreach ($customers as $customer) {
 
-        $changeInfo = array(
-            "500" => "0",
-            "100" => "0",
-            "50"  => "0",
-            "10"  => "0"
-        );
+            $arrCustomer = explode(" ", $customer);
 
-        //商品総価、各コイン入れた数
-        $price    = $arrCustomerInfo[0];
-        $coin_500 = $arrCustomerInfo[1];
-        $coin_100 = $arrCustomerInfo[2];
-        $coin_50  = $arrCustomerInfo[3];
-        $coin_10  = $arrCustomerInfo[4];
+            //お客様が購入した商品の価格及び入れた各コインの枚数
+            $productPrice = $arrCustomer[0];
+            $useCoin[500] = $arrCustomer[1];
+            $useCoin[100] = $arrCustomer[2];
+            $useCoin[50]  = $arrCustomer[3];
+            $useCoin[10]  = $arrCustomer[4];
 
-        //お釣り
-        $change = 500 * $coin_500 + 100 * $coin_100 + 50 * $coin_50 + 10 * $coin_10 - $price;
-        $old_change = $change;
-        $this->getChange(500, $change, $this->countOfCoin[500], $changeInfo[500]);
-        $this->getChange(100, $change, $this->countOfCoin[100], $changeInfo[100]);
-        $this->getChange(50, $change, $this->countOfCoin[50], $changeInfo[50]);
-        $this->getChange(10, $change, $this->countOfCoin[10], $changeInfo[10]);
+            $changeCoin = array(
+                "500" => "0",
+                "100" => "0",
+                "50"  => "0",
+                "10"  => "0"
+            );
 
+            //お釣り
+            $change = 500 * $useCoin[500] + 100 * $useCoin[100] + 50 * $useCoin[50] + 10 * $useCoin[10] - $productPrice;
+            $tmpChange = $change;
 
-        if ($changeInfo[50] *50 + $changeInfo[10] * 10 < 100
-            && $changeInfo[500] <= $this->countOfCoin[500]
-            && $changeInfo[100] <= $this->countOfCoin[100]
-            && $changeInfo[50] <= $this->countOfCoin[50]
-            && $changeInfo[10] <= $this->countOfCoin[10]
-            && ($changeInfo[500] * 500 + $changeInfo[100] * 100 + $changeInfo[50] * 50 + $changeInfo[10] * 10) == $old_change
-        ) {
-            //自動販売機内部コインの変化
-            $this->countOfCoin[500] = $this->countOfCoin[500] + $coin_500 - $changeInfo[500];
-            $this->countOfCoin[100] = $this->countOfCoin[100] + $coin_100 - $changeInfo[100];
-            $this->countOfCoin[50]  = $this->countOfCoin[50] + $coin_50 - $changeInfo[50];
-            $this->countOfCoin[10]  = $this->countOfCoin[10] + $coin_10 - $changeInfo[10];
-            //結果出力
-            $k = 0;
-            foreach ($changeInfo as $coin => $count) {
-                $message .= $count;
-                if ($k != count($changeInfo) - 1){
-                    $message .= " ";
-                }
-                $k++;
+            foreach ($this->coinInMachine as $coin => $coinNumber) {
+                $this->change($coin, $coinNumber, $change, $changeCoin[$coin]);
             }
-        } else {
-            $message .= "impossible";
+
+            $message = "";
+            if ($this->checkIfPass($changeCoin, $tmpChange)) {
+
+                //自動販売機内部コインの変化
+                foreach ($this->coinInMachine as $coin => $coinNumber) {
+                    $this->coinInMachine[$coin] = $coinNumber + $useCoin[$coin] - $changeCoin[$coin];
+                }
+
+                //出力
+                $k = 0;
+                foreach ($changeCoin as $coin => $count) {
+                    $message .= $count;
+                    if ($k == count($changeCoin) - 1) {
+                        break;
+                    }
+                    $message .= " ";
+                    $k++;
+                }
+
+            } else {
+                $message .= "impossible";
+            }
+            echo $message;
+            echo PHP_EOL;
         }
-        return $message .= PHP_EOL;
     }
 
-    public function getCustomerInfo($info_customer) : array
+    private function change(int $coin, int $coinNumber, int & $change, int & $changeCoin) : void
     {
-        $customerInfo = str_replace(array("\r\n","\r","\n"), '', $info_customer);
-        $arrCustomerInfo = explode(" ", $customerInfo);
-
-        return $arrCustomerInfo;
-    }
-
-    public function getChange($coin, & $change, & $countOfCoin, & $changeInfo)
-    {
-        for ($num = $countOfCoin; $num >= 0; $num--) {
+        if ($change < $coin) {
+            return;
+        }
+        for ($num = $coinNumber; $num >= 0; $num--) {
             $price = $change - $num * $coin;
             if ($price >= 0) {
                 $change = $change - $num * $coin;
-                $changeInfo = $num;
-                break;
+                $changeCoin = $num;
+                return;
             }
         }
     }
 
-    public function  getCountOfPeople() : int
+    private function checkIfPass(array $changeCoin, int $tmpChange) : bool
     {
-        return $this->countOfPeople;
+        if ($changeCoin[50] * 50 + $changeCoin[10] * 10 >= 100) {
+            return false;
+        }
+        foreach ($this->coinInMachine as $coin => $coinNumber) {
+            if ($changeCoin[$coin] > $coinNumber) {
+                return false;
+            }
+        }
+        if (($changeCoin[500] * 500 + $changeCoin[100] * 100 + $changeCoin[50] * 50 + $changeCoin[10] * 10 != $tmpChange)) {
+            return false;
+        }
+        return true;
     }
 }
 
-$B026 = new B026(trim(fgets(STDIN)), trim(fgets(STDIN)));
-for ($num = 1; $num <= $B026->getCountOfPeople(); $num++) {
-    echo $B026->sell(trim(fgets(STDIN)));
+
+$info = str_replace(array("\r\n","\r","\n"), '', trim(fgets(STDIN)));
+$arrInfo = explode(" ", $info);
+
+$coinInMachine[500] = $arrInfo[0];
+$coinInMachine[100] = $arrInfo[1];
+$coinInMachine[50]  = $arrInfo[2];
+$coinInMachine[10]  = $arrInfo[3];
+
+$countOfCustomer = str_replace(array("\r\n","\r","\n"), '', trim(fgets(STDIN)));
+$customers = array();
+for ($num = 1; $num <= $countOfCustomer; $num++) {
+    $customers[] = str_replace(array("\r\n","\r","\n"), '', trim(fgets(STDIN)));
 }
+
+$vendingMachine = new VendingMachine($coinInMachine);
+$vendingMachine->sellResult($customers);
